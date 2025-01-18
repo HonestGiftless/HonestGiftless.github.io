52aa437be9b11aaddbfed91de60ff8f1a7723624
Nikki <113893391+HonestGiftless@users.noreply.github.com>
2025-01-19 04:51:30 +0800

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('burger-checkbox');
     const navigation = document.querySelector('nav');
-    // const container = document.querySelector('CLASS');
+    const container = document.querySelector('main');
+    const footer = document.querySelector('footer');
 
     toggleBtn.addEventListener('click', () => {
         navigation.classList.toggle('open');
-        // container.classList.toggle('hidden');
+        container.classList.toggle('hidden');
+        footer.classList.toggle('hidden');
     });
 });
