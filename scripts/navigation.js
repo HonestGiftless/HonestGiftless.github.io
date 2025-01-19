e7314fe512eb8364be97b3faff3e6bcfc38d6136
Nikki <113893391+HonestGiftless@users.noreply.github.com>
2025-01-20 03:23:27 +0800

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -16,6 +16,7 @@ const listItems = document.querySelectorAll('nav > ul > li');
 listItems.forEach(item => {
     item.addEventListener('click', event => {
         const link = item.querySelector('a');
+        console.log(link);
 
         if (link) {
             window.location.href = link.href;
